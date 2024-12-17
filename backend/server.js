467fbbbdd03fcc24e7adad3c9a630ee272025a7c
Nirbhay-279 <nirbhaytiwari990@gmail.com>
2024-12-17 18:03:26 +0530

    / route :)

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Set up CORS
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173", // Allow requests from your frontend
     credentials: true, // Allow cookies to be sent with requests
-}));
+  })
+);
 
 const __dirname = path.resolve();
 
-app.use(express.json({limit:"10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 // ROUTING
@@ -35,17 +37,20 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+app.get("/", (req, res) => {
+  res.send("we are live");
+});
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    console.log("Server is running on http://localhost:" + PORT);
+  console.log("Server is running on http://localhost:" + PORT);
 
-    connectDB();
+  connectDB();
 });
