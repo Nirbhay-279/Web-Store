cfa20519a0cd2da09351fdc6635250667e17f00e
Nirbhay-279 <nirbhaytiwari990@gmail.com>
2025-01-18 16:02:55 +0530

    frontend fix :)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance =axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://web-store-frontend.vercel.app/api",
 	withCredentials: true, // send cookies to the server
 });
 export default axiosInstance;
